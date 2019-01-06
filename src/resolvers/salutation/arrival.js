d4cc90a476bfbca12a411d54e627dcf63cd07ef0
Wade Penistone <wadespackages@yahoo.com>
2019-01-06 14:45:57 +0000

    Switched to graphQL

--- src/resolvers/salutation/arrival.js
+++ src/resolvers/salutation/arrival.js
@@ -5,10 +5,17 @@ const randomItem = require('random-item');
   */
 module.exports = function(service, templater)
 {
-  return service.get('salutation', {
-    filter: {tags: 'ArrivalSalutation'}
-  }).then(res => {
-    let arrivalSalutation = randomItem(res.data).name;
+  let query = `{
+    salutations: allJsonApisalutation(filter: {tags: {in: "ArrivalSalutation"}}) {
+      edges {
+        node {
+          name
+        }
+      }
+    }
+  }`;
+  return service.request(query).then(data => {
+    let arrivalSalutation = randomItem(data.salutations.edges).node.name;
     return templater.tpl('salute', {arrivalSalutation}).compile();
   }).then(content => content.body)
     .catch(err => console.error(err));
--- src/resolvers/salutation/departure.js
+++ src/resolvers/salutation/departure.js
@@ -5,10 +5,17 @@ const randomItem = require('random-item');
   */
 module.exports = function(service, templater)
 {
-  return service.get('salutation', {
-    filter: {tags: 'DepartureSalutation'}
-  }).then(res => {
-    let departureSalutation = randomItem(res.data).name;
+  let query = `{
+    salutations: allJsonApisalutation(filter: {tags: {in: "DepartureSalutation"}}) {
+      edges {
+        node {
+          name
+        }
+      }
+    }
+  }`;
+  return service.request(query).then(data => {
+    let departureSalutation = randomItem(data.salutations.edges).node.name;
     return templater.tpl('salute', {departureSalutation}).compile();
   }).then(content => content.body)
     .catch(err => console.error(err));
